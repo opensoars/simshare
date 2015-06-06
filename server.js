e8b3a1b1284b5338a1ee66c4d1e315290e14365d
unknown <sam_1700@hotmail.com>
2015-06-06 14:36:29 +0200

    Small fixes

--- server.js
+++ server.js
@@ -1,45 +1,33 @@
 /**
- *
  * @namespace
  * @desc Top level app object
- *
  */
 var app = {};
 
-
 /**
- *
  * @namespace
  * @desc Application configuration
- *
  */
 app.config = {
   http_port: 3333
 };
 
-
 /**
- *
  * @namespace
  * @desc App memory database
- *
  */
 app.db = {
   text_data: ''
 };
 
-
 /**
- *
  * @namespace
  * @desc App module object
- *
  */
 app.modules = {
   express: require('express'),
   Ezlog: require('ezlog')
 };
-
 
 /**
  *
@@ -52,15 +40,30 @@ app.libs = {
 };
 
 
-// Initialize express
+/**
+ *
+ * Initialize application logic
+ *
+ */
+
+
+/**
+ * Initialize express
+ */
 app.express = app.modules.express();
 
-// Middleware stack
+/**
+ * Middleware stack
+ */
 app.express.use(app.modules.express.static(__dirname + '/public'));
 
-// Setup application routes
+/**
+ * Setup application routes
+ */
 app.express.post('/text_data', app.libs.http_handlers.post_data);
 app.express.get('/text_data', app.libs.http_handlers.get_data);
 
-// Start listening for http requests
+/**
+ * Start listening for http requests
+ */
 app.express.listen(app.config.http_port);
